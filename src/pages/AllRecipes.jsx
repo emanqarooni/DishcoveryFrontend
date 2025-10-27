9383756de1e65f9c31f47ebb60e1bf2d2d5297df
Fatima-Zaid <fatimaameer957@gmail.com>
2025-10-27 13:00:38 +0300

    get recipes in all recipes page

--- src/pages/AllRecipes.jsx
+++ src/pages/AllRecipes.jsx
@@ -1,8 +1,16 @@
-const { useState } = require("react")
+const { useState, useEffect } = require("react")
+import { BASE_URL, Client } from "../services/api.js"
 
 const AllRecipes = () => {
+  const [recipes, setRecipes] = useState([])
 
+  useEffect(() => {
+    const getRecipes = async () => {
+      try {
+        const response = await Client.get(`${BASE_URL}/recipe`)
+      } catch (error) {}
+    }
+  }, [])
 }
-
 
 export default AllRecipes
